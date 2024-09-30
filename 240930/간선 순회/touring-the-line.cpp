130965ca50be2dbca1f5345dad12e8c8505a48c1
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-09-30 16:40:25 +0900

    Title: 간선 순회, Time: 120ms, Memory: 4MB, Status: Passed - CodeTree

--- 240930/간선 순회/touring-the-line.cpp
+++ 240930/간선 순회/touring-the-line.cpp
@@ -16,29 +16,34 @@ int main() {
     }
 
     vector<int> dist(N+1, -1);
+    vector<int> depth(N+1, -1);
 
     function<void(int)> dfs = [&](int cur){
         for(auto &[nxt, cost] : E[cur]){
             if(dist[nxt] >= 0) continue;
             dist[nxt] = dist[cur] + cost;
+            depth[nxt] = depth[cur] + 1;
             dfs(nxt);
         }
     };
 
     dist[1] = 0;
+    depth[1] = 0;
     dfs(1);
 
     int mxidx = 1;
     for(int i = 1; i <= N; i++){
-        if(dist[mxidx] <= dist[i]) mxidx = i;
+        if(depth[mxidx] <= depth[i]) mxidx = i;
     }
 
     fill(dist.begin(), dist.end(), -1);
+    fill(depth.begin(), depth.end(), -1);
     dist[mxidx] = 0;
+    depth[mxidx] = 0;
     dfs(mxidx);
 
     for(int i = 1; i <= N; i++){
-        if(dist[mxidx] <= dist[i]) mxidx = i;
+        if(depth[mxidx] <= depth[i]) mxidx = i;
     }
 
     cout << (dist[mxidx] + D-1)/D << "\n";
