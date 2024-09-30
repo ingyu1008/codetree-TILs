2b1770cc7ff8d8cdc34e0265b134fbb814d5808f
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-09-30 16:37:10 +0900

    Title: 간선 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/간선 순회/touring-the-line.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, D;
+    cin >> N >> D;
+
+    vector<vector<pair<int, int>>> E(N+1);
+
+    for(int i = 1; i < N; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        E[u].push_back({v, w});
+        E[v].push_back({u, w});
+    }
+
+    vector<int> dist(N+1, -1);
+
+    function<void(int)> dfs = [&](int cur){
+        for(auto &[nxt, cost] : E[cur]){
+            if(dist[nxt] >= 0) continue;
+            dist[nxt] = dist[cur] + cost;
+            dfs(nxt);
+        }
+    };
+
+    dist[1] = 0;
+    dfs(1);
+
+    int mxidx = 1;
+    for(int i = 1; i <= N; i++){
+        if(dist[mxidx] <= dist[i]) mxidx = i;
+    }
+
+    fill(dist.begin(), dist.end(), -1);
+    dist[mxidx] = 0;
+    dfs(mxidx);
+
+    for(int i = 1; i <= N; i++){
+        if(dist[mxidx] <= dist[i]) mxidx = i;
+    }
+
+    cout << (dist[mxidx] + D-1)/D << "\n";
+
+    return 0;
+}
