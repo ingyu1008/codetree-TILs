f3bcff2a2aaab1e67025e8fa437bab435efe55a1
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-10-05 12:06:13 +0900

    Title: 트리 순회법 변경, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 241005/트리 순회법 변경/change-tree-traversal.cpp
+++ 241005/트리 순회법 변경/change-tree-traversal.cpp
@@ -5,38 +5,35 @@ int main() {
     int N;
     cin >> N;
 
-    vector<int> A(N);
+    vector<int> A(N), B(N, 1e9);
 
     for(int i = 0; i < N; i++) cin >> A[i];
 
-    vector<pair<int, int>> E(N, {-1, -1});
-    vector<int> par(N);
+    stack<int> monotone;
 
-    int cur = 0;
-    
-    for(int i = 1; i < N; i++){
-        if(A[i] < A[cur]){
-            E[cur].first = i;
-            par[i] = cur;
-            cur = i;
-        } else if(cur == 0 || A[i] < A[par[cur]]){
-            E[cur].second  = i;
-            par[i] = cur;
-            cur = i;
-        } else{
-            while(cur != 0 && A[i] > A[par[cur]]) cur = par[cur];
-            E[cur].second  = i;
-            par[i] = cur;
-            cur = i;
+    for(int i = 0; i < N; i++){
+        
+        while(!monotone.empty() && A[monotone.top()] < A[i]){
+            B[monotone.top()] = i;
+            monotone.pop();
         }
+
+        monotone.push(i);
     }
-    function<void(int)> dfs = [&](int cur){
-        if(E[cur].first >= 0) dfs(E[cur].first);
-        if(E[cur].second >= 0) dfs(E[cur].second);
-        cout << A[cur] << "\n";
+
+    function<void(int, int)> dfs = [&](int l, int r){
+        if( l > r) return;
+        if(B[l] == 1e9){
+            dfs(l+1, r);
+        } else {
+            dfs(l+1, B[l]-1);
+            dfs(B[l], r);
+        }
+        cout << A[l] << "\n";
+        return;
     };
 
-    dfs(0);
+    dfs(0, N-1);
 
     return 0;
 }
