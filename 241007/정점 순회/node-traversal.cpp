90c25075eeac0b624d834a9820533c5ec432c3b2
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-10-07 14:45:44 +0900

    Title: 정점 순회, Time: 97ms, Memory: 5MB, Status: Wrong Answer - CodeTree

---
+++ 241007/정점 순회/node-traversal.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, S, D;
+    cin >> N >> S >> D;
+
+    vector<vector<int>> E(N+1);
+    for(int i = 1; i < N; i++){
+        int u, v;
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    vector<int> depth(N+1);
+
+    function<void(int, int)> dfs = [&](int cur, int par){
+        for(auto &nxt: E[cur]){
+            if(nxt == par) continue;
+            dfs(nxt, cur);
+            depth[cur] = max(depth[cur], depth[nxt]);
+        }
+        depth[cur]++;
+    };
+
+    int ans = 0;
+
+    function<void(int, int)> dfs2 = [&](int cur, int par){
+        if(depth[cur] <= D){
+            ans--;
+            return;
+        }
+        for(auto &nxt: E[cur]){
+            if(nxt == par) continue;
+            dfs2(nxt, cur);
+            ans++;
+        }
+        ans++;
+    };
+
+    dfs(S, 0);
+    dfs2(S, 0);
+
+    cout << ans - 1 <<"\n";
+
+
+    
+    return 0;
+}
