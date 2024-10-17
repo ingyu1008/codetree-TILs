537ff05a5e6f7aadf9a08df6dd0a9fe873fe8150
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-10-18 03:45:24 +0900

    Title: 연결된 정점, Time: 179ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241018/연결된 정점/connected-vertex.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct UF {
+    vector<int> par;
+    vector<int> sz;
+    vector<int> rank;
+
+    UF(int N): par(N+1), sz(N+1, 1), rank(N+1, 1){
+        iota(par.begin(), par.end(), 0);
+    }
+
+    int find(int u){
+        if(u >= par.size()) return u;
+        if(u == par[u]) return u;
+
+        return par[u] = find(par[u]);
+    }
+
+    void unite(int u, int v){
+        u = find(u);
+        v = find(v);
+
+        if(u == v) return;
+
+        if(rank[u] > rank[v]) swap(u, v);
+
+        par[u] = v;
+        sz[v] += sz[u];
+        if(rank[u] == rank[v]) rank[v]++;
+    }
+};
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    UF uf(N);
+
+    for(int i = 0; i < M; i++){
+        string op;
+        cin >> op;
+        if(op == "x"){
+            int a, b;
+            cin >> a >> b;
+            uf.unite(a, b);
+        } else{
+            int a;
+            cin >> a;
+            cout << uf.sz[uf.find(a)] << "\n";
+        }
+    }
+
+    return 0;
+}
