6454d39ef0dfef9773d3a3fd504784e91c9071e1
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-10-18 03:41:20 +0900

    Title: 경로의 적합성 판단 2, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241018/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct UF {
+    vector<int> par;
+    vector<int> sz;
+    vector<int> rank;
+
+    UF(int N): par(N+1), sz(N+1, 1), rank(N+1, 1){
+        iota(par.begin(), par.end(), 0);
+    }
+
+    int find(int u){
+        if(u >= par.size()) return u;
+        if(u == par[u]) return u;
+
+        return par[u] = find(par[u]);
+    }
+
+    void unite(int u, int v){
+        u = find(u);
+        v = find(v);
+
+        if(u == v) return;
+
+        if(rank[u] > rank[v]) swap(u, v);
+
+        par[u] = v;
+        sz[v] += sz[u];
+        if(rank[u] == rank[v]) rank[v]++;
+    }
+};
+
+int main() {
+    int N, M, K;
+
+    cin >> N >> M;
+
+    UF uf(N);
+
+    for(int i = 0; i < M; i++){
+        int u, v;
+        cin >> u >> v;
+        uf.unite(u, v);
+    }   
+
+    int u;
+    cin >> u;
+
+    for(int i = 1; i < K; i++){
+        int v;
+        cin >> v;
+        if(uf.find(u) != uf.find(v)){
+            cout << "0\n";
+            exit(0);
+        }
+    }
+
+    cout << "1\n";
+
+    return 0;
+}
