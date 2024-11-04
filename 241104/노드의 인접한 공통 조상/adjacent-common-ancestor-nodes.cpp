3270a14b46a02e0b7e54d7a1498e1e4091039ae4
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-11-04 16:16:58 +0900

    Title: 노드의 인접한 공통 조상, Time: 13ms, Memory: 2MB, Status: Passed - CodeTree

--- 241104/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.cpp
+++ 241104/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.cpp
@@ -9,38 +9,73 @@ int main() {
     int N;
     cin >> N;
 
-    vector<vector<int>> par(N+1, vector<int>(20, -1));
-
+    vector<int> par(N+1, -1);
+    vector<vector<int>> E(N+1);
     for(int i = 0; i < N-1; i++){
         int a, b;
         cin >> a >> b;
-        par[b][0] = a;
+        par[b] = a;
+        E[a].push_back(b);
     }
 
-    for(int j = 1; j < 20; j++){
-        for(int i = 1; i <= N; i++){
-            if(par[i][j-1] != -1){
-                par[i][j] = par[par[i][j-1]][j-1];
-            }
+    int root;
+    for(int i = 1; i <= N; i++){
+        if(par[i] == -1){
+            root = i;
+            break;
+        }
+    }
+
+    vector<int> depth(N+1);
+    vector<int> euler;
+    vector<int> first(N+1, -1);
+
+    function<void(int, int)> dfs = [&](int cur, int d){
+        depth[cur] = d;
+        euler.push_back(cur);
+        if(first[cur] == -1){
+            first[cur] = euler.size()-1;
+        }
+
+        for(auto next : E[cur]){
+            dfs(next, d+1);
+            euler.push_back(cur);
+        }
+    };
+
+    dfs(root, 0);
+
+    vector<vector<int>> sparse_table(log2(euler.size()) + 10, vector<int>(euler.size()));
+
+    for(int i = 0; i < euler.size(); i++){
+        sparse_table[0][i] = euler[i];
+    }
+
+    for(int i = 1; i < sparse_table.size(); i++){
+        for(int j = 0; j + (1 << i) <= euler.size(); j++){
+            int a = sparse_table[i-1][j];
+            int b = sparse_table[i-1][j + (1 << (i-1))];
+            sparse_table[i][j] = depth[a] < depth[b] ? a : b;
         }
     }
 
     int u, v;
     cin >> u >> v;
 
-    if(u == v){
-        cout << u << '\n';
-        return 0;
+    int l = first[u];
+    int r = first[v];
+
+    if(l > r){
+        swap(l, r);
     }
 
-    for(int j = 19; j >= 0; j--){
-        if(par[u][j] != -1 && par[u][j] != par[v][j]){
-            u = par[u][j];
-            v = par[v][j];
-        }
-    }
+    int k = log2(r-l+1);
 
-    cout << par[u][0] << '\n';
+    int a = sparse_table[k][l];
+    int b = sparse_table[k][r - (1 << k) + 1];
+
+    cout << (depth[a] < depth[b] ? a : b) << '\n';
+    
 
 
     return 0;
