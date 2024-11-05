67e485d24243684667662b77c06aaada11c5e750
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-11-05 09:59:17 +0900

    Title: 최소 스패닝 트리 9, Time: 106ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241105/최소 스패닝 트리 9/minimum-spanning-tree-9.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    cin.tie(0)->sync_with_stdio();
+    
+    int N, M;
+    cin >> N >> M;
+
+    vector<array<int, 3>> E;
+
+    for(int i = 0; i < M; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        E.push_back({c, a, b});
+    }
+
+    sort(E.begin(), E.end());
+
+    vector<int> par(N+1);
+
+    iota(par.begin(), par.end(), 0);
+
+    function<int(int)> find = [&](int x) {
+        if(par[x] == x) return x;
+        return par[x] = find(par[x]);
+    };
+
+    auto unite = [&](int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a != b) par[a] = b;
+    };
+
+    ll mst = 0;
+
+    for(auto [c, a, b] : E){
+        if(find(a) == find(b)) continue;
+        unite(a, b);
+        mst += c;
+    }
+
+    cout << mst << '\n';
+
+
+    return 0;
+}
