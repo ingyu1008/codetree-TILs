d6c7cbf839a03c90b37b0675fd47d5ca2b6708b3
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-11-26 14:55:42 +0900

    Title: 노드 맞추기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241126/노드 맞추기/nodes-guessing.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio();
+    
+    int N;
+    cin >> N;
+
+    map<string, int> names;
+    map<int, string> inames;
+    for (int i = 0; i < N; i++)
+    {
+        string name;
+        cin >> name;
+        names[name] = i;
+        inames[i] = name;
+    }
+
+    int M;
+    cin >> M;
+
+    vector<vector<int>> E(N);
+    vector<int> indegree(N);
+
+    for (int i = 0; i < M; i++)
+    {
+        string a, b;
+        cin >> a >> b;
+        
+        int u = names[a], v = names[b];
+
+        E[v].push_back(u);
+        indegree[u]++;
+    }
+
+    vector<int> roots;
+    vector<vector<int>> children(N);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < N; i++)
+    {
+        if (indegree[i] == 0){
+            pq.push(i);
+            roots.push_back(i);
+        }
+    }
+
+    while (!pq.empty())
+    {
+        int u = pq.top();
+        pq.pop();
+
+        for (int v : E[u])
+        {
+            indegree[v]--;
+            if (indegree[v] == 0)
+            {
+                pq.push(v);
+                children[u].push_back(v);
+            }
+        }
+    }
+
+    cout << roots.size() << '\n';
+
+    for (int root : roots)
+    {
+        cout << inames[root] << ' ';
+    }
+    cout << '\n';
+    
+    for(auto &[name, idx]: names){
+        cout << name << ' ' << children[idx].size() << ' ';
+        for(int child: children[idx]){
+            cout << inames[child] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
