d63aecb006f97734063340621ffe5ae52cd7e562
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-11-27 15:19:33 +0900

    Title: 겹치는 bit가 없는 세 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241127/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.cpp
+++ 241127/겹치는 bit가 없는 세 수/three-numbers-with-no-overlapping-bits.cpp
@@ -6,7 +6,7 @@ typedef long long ll;
 int main()
 {
     cin.tie(0)->sync_with_stdio();
-    
+
     int N;
     cin >> N;
 
@@ -17,24 +17,22 @@ int main()
         cin >> A[i];
     }
 
-    ll ans = 1e18;
+    ll ans = 0;
 
-    for(int i = 0; i < N; i++){
-        for(int j = i+1; j < N; j++){
-            for(int k = j+1; k < N; k++){
-                if(!(A[i] & A[j]) && !(A[j] & A[k]) && !(A[k] & A[i])){
-                    ans = min(ans, 1LL * A[i] + A[j] + A[k]);
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            for (int k = j + 1; k < N; k++)
+            {
+                if (!(A[i] & A[j]) && !(A[j] & A[k]) && !(A[k] & A[i]))
+                {
+                    ans = max(ans, 1LL * A[i] + A[j] + A[k]);
                 }
             }
         }
     }
-
-    if(ans == 1e18){
-        cout << 0 << '\n';
-    }else {
-        cout << ans << '\n';
-    }
-
+    cout << ans << '\n';
 
     return 0;
 }
