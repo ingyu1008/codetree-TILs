5861ed68977f7319fcb756232321c0c945ed3e6d
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-11-27 14:44:13 +0900

    Title: 새로 번호 매기기, Time: 47ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 241127/새로 번호 매기기/renumbering-process.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio();
+    
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> E(N);
+    vector<int> in(N);
+
+    for(int i = 0; i < M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+
+        E[a].push_back(b);
+        in[b]++;
+    }
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    for(int i = 0; i < N; i++)
+        if(in[i] == 0)
+            pq.push(i);
+        
+    vector<int> order;
+    while(!pq.empty())
+    {
+        int u = pq.top();
+        pq.pop();
+
+        order.push_back(u);
+
+        for(int v : E[u])
+        {
+            in[v]--;
+            if(in[v] == 0)
+                pq.push(v);
+        }
+    }
+
+    if(order.size() != N)
+    {
+        cout << "-1\n";
+        return 0;
+    }
+
+    vector<int> ans(N);
+
+    for(int i = 0; i < N; i++)
+        ans[order[i]] = i + 1;
+
+    for(int i = 0; i < N; i++)
+        cout << ans[i] << " ";
+
+    return 0;
+}
