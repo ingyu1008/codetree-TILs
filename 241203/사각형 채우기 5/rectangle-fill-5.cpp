ac75fef6473ae299b0b70af9be48de1fc64ba253
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-12-03 09:41:38 +0900

    Title: 사각형 채우기 5, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241203/사각형 채우기 5/rectangle-fill-5.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio();
+
+    int N;
+    cin >> N;
+
+    vector<vector<int>> dp(4*N, vector<int>((1 << 4), -1));
+
+    int mod = 10007;
+
+    function<int(int, int)> solve = [&](int idx, int mask) -> int {
+        if(idx == 4*N) return mask == 0;
+        if(idx > 4*N) return 0;
+        if(dp[idx][mask] != -1) return dp[idx][mask];
+
+        if(mask & 1) return dp[idx][mask] = solve(idx + 1, mask >> 1);
+
+        int ret = solve(idx + 1, (mask >> 1) + (1 << 3));
+
+        if((mask & 4) == 0 && idx % 4 != 3){
+            ret += solve(idx + 2, (mask >> 3));
+        }
+
+        return dp[idx][mask] = ret % mod;
+    };
+
+    cout << solve(0, 0) << '\n';
+
+    return 0;
+}
