d9d1b79afd72868d0b3bf1d12f0acd9bc0e810b8
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-12-03 14:52:04 +0900

    Title: 접두사와 자동완성, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241203/접두사와 자동완성/prefix-and-autocomplete.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+struct Node {
+    char c;
+    int depth;
+    int cnt;
+    vector<int> nxt;
+    int numchild;
+    Node(char c = '$', int depth = 0) : c(c), depth(depth), cnt(0), numchild(0) {
+        nxt = vector<int>(26, -1);
+    }
+};
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio();
+
+    int N;
+    cin >> N;
+
+    vector<Node> trie(1);
+
+    auto add = [&](string &s) {
+        int cur = 0;
+        for (char c : s) {
+            if (trie[cur].nxt[c - 'a'] == -1) {
+                trie[cur].nxt[c - 'a'] = trie.size();
+                trie.emplace_back(c, trie[cur].depth + 1);
+                trie[cur].numchild++;
+            }
+            cur = trie[cur].nxt[c - 'a'];
+            trie[cur].cnt++;
+        }
+    };
+    
+    vector<string> S(N);
+    
+    for(int i = 0; i < N; i++){
+        cin >> S[i];
+        add(S[i]);
+    }
+
+    for(int i = 0; i < N; i++){
+        int sz = S[i].size();
+        int cur = 0;
+        int ans = 0;
+        for(int j = 0; j < sz; j++){
+            if(trie[cur].numchild > 1){
+                ans++;
+            }
+            cur = trie[cur].nxt[S[i][j] - 'a'];
+        }
+        cout << ans << ' ';
+    }
+    
+    
+
+    return 0;
+}
