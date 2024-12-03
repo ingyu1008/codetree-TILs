3ffabc686560bbe2465f23ebe1b8cf653542db2b
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-12-03 15:32:37 +0900

    Title: 문자열을 채워 넣는 가짓수, Time: 6ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241203/문자열을 채워 넣는 가짓수/number-of-ways-to-fill-a-string.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+struct Node {
+    char c;
+    int depth;
+    vector<int> nxt;
+    Node(char c='$', int depth=0) : c(c), depth(depth) {
+        nxt = vector<int>(27, -1);
+    }
+};
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio();
+    
+    string T;
+    cin >> T;
+
+    int N;
+    cin >> N;
+
+    vector<Node> trie(1);
+
+    auto add = [&](string &s) {
+        int cur = 0;
+        for(int i = 0; i < s.size(); i++){
+            int c = s[i] - 'a';
+            if(trie[cur].nxt[c] == -1){
+                trie[cur].nxt[c] = trie.size();
+                trie.push_back(Node(s[i], i+1));
+            }
+            cur = trie[cur].nxt[c];
+        }
+    };
+
+    for(int i = 0; i < N; i++){
+        string s;
+        cin >> s;
+        s += 'z' + 1;
+        add(s);
+    }
+
+    T = '$' + T;
+
+    vector<int> dp(T.size(), 0);
+    dp[0] = 1;
+
+    for(int i = 1; i < T.size(); i++){
+        int cur = 0;
+        for(int j = i; j < T.size(); j++){
+            int c = T[j] - 'a';
+            if(trie[cur].nxt[c] == -1) break;
+            cur = trie[cur].nxt[c];
+            if(trie[cur].nxt[26] != -1){
+                dp[j] += dp[i-1];
+            }
+        }
+    }
+
+    cout << dp[T.size()-1] << '\n';
+
+    return 0;
+}
