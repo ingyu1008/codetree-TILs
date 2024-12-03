1255c340b663a1a831e7ef67ad0d843a4571d2f7
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2024-12-03 10:39:02 +0900

    Title: 중복되는 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241203/중복되는 수열/duplicate-sequence.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+struct Node {
+    vector<int> nxt;
+    Node() {
+        nxt = vector<int>(10, -1);
+    }
+};
+
+int main()
+{
+    cin.tie(0)->sync_with_stdio();
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<Node> trie(1);
+
+    auto add = [&](string &s) {
+        int cur = 0;
+        for (char c : s) {
+            if (trie[cur].nxt[c - '0'] == -1) {
+                trie[cur].nxt[c - '0'] = trie.size();
+                trie.emplace_back();
+            }
+            cur = trie[cur].nxt[c - '0'];
+        }
+    };
+
+    auto find = [&](string &s) {
+        int cur = 0;
+        for (char c : s) {
+            if (trie[cur].nxt[c - '0'] == -1) {
+                return false;
+            }
+            cur = trie[cur].nxt[c - '0'];
+        }
+        
+        for(int i = 0; i < 10; i++) {
+            if(trie[cur].nxt[i] != -1) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    vector<string> S(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> S[i];
+        add(S[i]);
+    }
+
+    int ans = 1;
+
+    for (int i = 0; i < M; i++) {
+        string s;
+        cin >> s;
+        if(find(s)) {
+            ans = 0;
+        }
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
