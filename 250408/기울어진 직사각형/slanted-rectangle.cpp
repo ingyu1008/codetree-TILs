b6532aaf8124b58c6c7e44a796845ec4e4adfcaa
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2025-04-08 10:20:21 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250408/기울어진 직사각형/slanted-rectangle.cpp
+++ 250408/기울어진 직사각형/slanted-rectangle.cpp
@@ -65,6 +65,55 @@ int main(int argc, char **argv)
         }
     }
 
+    for(int dx = 1; dx < N; dx++)
+    {
+        for(int dy = 1; dy < N; dy++)
+        {
+            for(int r = 0; r < N; r++)
+            {
+                for(int c = 0; c < N; c++)
+                {
+                    // start at (r, c)
+                    // (+1, +1) by dx
+                    // (+1, -1) by dy
+                    // (-1, -1) by dx
+                    // (-1, +1) by dy
+
+                    if((r + dx + dy >= N) || (c + dx >= N) || (c + dx+-dy-dy < 0))
+                        continue;
+                    
+                    int sum = 0;
+                    int cr = r;
+                    int cc = c;
+                    int dx1 = dx;
+                    int dy1 = dy;
+                    while(dx1 > 0){
+                        cr++; cc++;
+                        sum += grid[cr][cc];
+                        dx1--;
+                    }
+                    while(dy1 > 0){
+                        cr++; cc--;
+                        sum += grid[cr][cc];
+                        dy1--;
+                    }
+                    while(dx1 < dx){
+                        cr--; cc--;
+                        sum += grid[cr][cc];
+                        dx1++;
+                    }
+                    while(dy1 < dy){
+                        cr--; cc++;
+                        sum += grid[cr][cc];
+                        dy1++;
+                    }
+                    
+                    ans = max(ans, sum);
+                }
+            }
+        }
+    }
+
     cout << ans << endl;
 
     return 0;
