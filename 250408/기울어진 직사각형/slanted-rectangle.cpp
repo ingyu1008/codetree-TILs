69b2555b9e67c289d5f2dbb8807e498263ef0936
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2025-04-08 10:24:26 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250408/기울어진 직사각형/slanted-rectangle.cpp
+++ 250408/기울어진 직사각형/slanted-rectangle.cpp
@@ -30,7 +30,17 @@ int main(int argc, char **argv)
                     // (-1, +1) by dx
                     // (-1, -1) by dy
 
-                    if((r + dx + dy >= N) || (c - dx < 0) || (c - dx+dy+dy  >= N))
+                    //(r,c)
+                    if(r >= N || c >= N || r < 0 || c < 0)
+                        continue;
+                    //(r+dx, c-dx)
+                    if(r + dx >= N || c - dx < 0 || r + dx < 0 || c - dx >= N)
+                        continue;
+                    //(r+dx+dy, c-dx+dy)
+                    if(r + dx + dy >= N || c - dx + dy < 0 || r + dx + dy < 0 || c - dx + dy >= N)
+                        continue;
+                    //(r+dy, c+dy)
+                    if(r + dy >= N || c + dy < 0 || r + dy < 0 || c + dy >= N)
                         continue;
                     
                     int sum = 0;
@@ -64,56 +74,6 @@ int main(int argc, char **argv)
             }
         }
     }
-
-    for(int dx = 1; dx < N; dx++)
-    {
-        for(int dy = 1; dy < N; dy++)
-        {
-            for(int r = 0; r < N; r++)
-            {
-                for(int c = 0; c < N; c++)
-                {
-                    // start at (r, c)
-                    // (+1, +1) by dx
-                    // (+1, -1) by dy
-                    // (-1, -1) by dx
-                    // (-1, +1) by dy
-
-                    if((r + dx + dy >= N) || (c + dx >= N) || (c + dx+-dy-dy < 0))
-                        continue;
-                    
-                    int sum = 0;
-                    int cr = r;
-                    int cc = c;
-                    int dx1 = dx;
-                    int dy1 = dy;
-                    while(dx1 > 0){
-                        cr++; cc++;
-                        sum += grid[cr][cc];
-                        dx1--;
-                    }
-                    while(dy1 > 0){
-                        cr++; cc--;
-                        sum += grid[cr][cc];
-                        dy1--;
-                    }
-                    while(dx1 < dx){
-                        cr--; cc--;
-                        sum += grid[cr][cc];
-                        dx1++;
-                    }
-                    while(dy1 < dy){
-                        cr--; cc++;
-                        sum += grid[cr][cc];
-                        dy1++;
-                    }
-                    
-                    ans = max(ans, sum);
-                }
-            }
-        }
-    }
-
     cout << ans << endl;
 
     return 0;
