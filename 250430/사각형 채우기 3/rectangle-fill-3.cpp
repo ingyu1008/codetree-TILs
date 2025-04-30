0cde20e0d80dd98ca36703964020d27daa2cf30a
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2025-04-30 14:07:40 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250430/사각형 채우기 3/rectangle-fill-3.cpp
+++ 250430/사각형 채우기 3/rectangle-fill-3.cpp
@@ -10,14 +10,14 @@ int main(int argc, char **argv)
     int N;
     cin >> N;
 
-    vector<int> dp(N+1);
+    vector<ll> dp(N+1);
 
     dp[0] = 1;
     dp[1] = 2;
     dp[2] = 7;
 
     for(int i = 3; i <= N; i++){
-        dp[i] = (dp[i-2]*4 + dp[i-1]*2) % 10007;
+        dp[i] = (dp[i-2]*4 + dp[i-1]*2) % (1e9+7);
     }
 
     cout << dp[N] << "\n";
