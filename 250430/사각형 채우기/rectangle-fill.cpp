ba3519c43c2ca6cb30728f78bfd3ce5ad3f76ee2
Ingyu Bang <ingyu1008@hanyang.ac.kr>
2025-04-30 14:06:12 +0900

    Title: 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250430/사각형 채우기/rectangle-fill.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(int argc, char **argv)
+{
+    cin.tie(0)->sync_with_stdio(false);
+    
+    int N;
+    cin >> N;
+
+    vector<int> dp(N+1);
+
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(int i = 3; i <= N; i++){
+        dp[i] = (dp[i-2] + dp[i-1]) % 10007;
+    }
+
+    cout << dp[N] << "\n";
+
+    return 0;
+}
